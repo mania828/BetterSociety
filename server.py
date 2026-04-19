a4513c13d31fcd8889f9fc2ebd8f0fd47e51259e
mania828 <maniaworksstudios@gmail.com>
2026-04-18 18:10:24 -0700

    Update server.py

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI, WebSocket
-
+import asyncio
 app = FastAPI()
 
-clients = []
+clients = set()
 
 @app.get("/")
 def home():
@@ -11,15 +11,28 @@ def home():
 @app.websocket("/ws")
 async def ws(websocket: WebSocket):
     await websocket.accept()
-    clients.append(websocket)
+    clients.add(websocket)
 
     try:
         while True:
             msg = await websocket.receive_text()
 
-            for c in clients:
-                if c != websocket:
+            for c in list(clients):
+                try:
                     await c.send_text(msg)
+                except:
+                    clients.discard(c)
+                    asyncio.create_task(keep_alive(websocket))
 
     except:
-        clients.remove(websocket)
+        clients.discard(websocket)
+
+        
+
+async def keep_alive(websocket: WebSocket):
+    while True:
+        try:
+            await websocket.send_text("__ping__")
+            await asyncio.sleep(20)
+        except:
+            break
